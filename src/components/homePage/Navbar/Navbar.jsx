926878924631663b73aa9ee0c50a46863317d2a1
Akul Varshney <allenpvarshney@gmail.com>
2022-05-11 23:53:57 +0530

    firebase deployed

--- src/components/homePage/Navbar/Navbar.jsx
+++ src/components/homePage/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./navbar.css";
 import Moon from "../../img/moon.png";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const icon = document.getElementById("icon");
+  console.log(icon);
 
   const clicked = () => {
     document.body.classList.toggle("dark-theme");
@@ -13,9 +13,9 @@ const Navbar = () => {
     <div>
       <nav className="container">
         <div className="nav-area">
-          <div className="left-nav">Light Mode</div>
+          <div className="left-nav">Light Theme</div>
           <img src={Moon} id="icon" alt="kuch" onClick={clicked} />
-          <div className="right-nav">Dark Mode</div>
+          <div className="right-nav">Dark Theme</div>
         </div>
       </nav>
     </div>
--- src/components/homePage/Team/Team.jsx
+++ src/components/homePage/Team/Team.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./team.css";
 import Akul from "../../img/allenv213@gmail.com.png";
 import Adityansh from "../../img/adityansh.sharma@s.amity.edu.png";
-import { RiFacebookCircleFill } from "react-icons/ri";
 import { AiFillLinkedin } from "react-icons/ai";
 
 const Team = () => {
@@ -16,12 +15,6 @@ const Team = () => {
               <div className="img-box">
                 <img src={Akul} className="img-responsive" alt="kuch" />
                 <ul>
-                  <a href="#">
-                    <li>
-                      <RiFacebookCircleFill />
-                    </li>
-                  </a>
-
                   <a href="https://www.linkedin.com/in/akul-v-6a37bb110/">
                     <li>
                       <AiFillLinkedin />
@@ -35,14 +28,8 @@ const Team = () => {
             </div>
             <div className="team-card profile-pic">
               <div className="img-box">
-                <img src={Adityansh} className="img-responsive" />
+                <img src={Adityansh} className="img-responsive" alt="kuch " />
                 <ul>
-                  <a href="#">
-                    <li>
-                      <RiFacebookCircleFill />
-                    </li>
-                  </a>
-
                   <a href="https://www.linkedin.com/in/adityansh-sharma-7646241a4/">
                     <li>
                       <AiFillLinkedin />
